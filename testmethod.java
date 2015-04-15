46d2a688e471f99de712d129ae8c7d2919908756
Ryan Mohr <rmohr92@gmail.com>
2015-04-15 12:03:53 -0400

    Added testmethod.java

---
+++ testmethod.java
@@ -0,0 +1,21 @@
+public class testmethod
+{
+	/**
+	 * [main description]
+	 * @author [name]
+	 * @description [description]
+	 * @datetime    2015-04-03T11:29:11-0500
+	 * @param       args[]                   [description]
+	 */
+	
+	/**
+	 * [main description]
+	 * @param args[] [description]
+	 */
+	public static void main(String args[])
+	{
+		Object o = new Object();
+		System.out.println(o);
+	}
+
+}
